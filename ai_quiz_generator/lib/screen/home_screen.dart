d8b1f9f147a34d117f7d3f2d284f70fbc9fd61f2
Kyran-Git <niklauscareer@gmail.com>
2025-11-19 22:42:47 +0800

    Refactor HomeScreen layout for improved gesture handling; add google_generative_ai dependency

--- ai_quiz_generator/lib/screen/home_screen.dart
+++ ai_quiz_generator/lib/screen/home_screen.dart
@@ -11,38 +11,46 @@ class _HomeScreenState extends State<HomeScreen> {
   final _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Home")),
-      body: bodyContainer(),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(title: const Text("Home")),
+        body: bodyContainer(),
+      ),
     );
   }
 
   Widget bodyContainer() {
-    return Container(
-      padding: EdgeInsets.all(8.0),
-      child: Form(
-        key: _formkey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextFormField(
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if (_formkey.currentState!.validate()) {
-                  // Process data.
-                }
-              },
-              child: Text("GENERATE QUIZ"),
-            ),
-          ],
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        padding: EdgeInsets.all(8.0),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formkey.currentState!.validate()) {
+                    // Process data.
+                  }
+                },
+                child: Text("GENERATE QUIZ"),
+              ),
+            ],
+          ),
         ),
       ),
     );
