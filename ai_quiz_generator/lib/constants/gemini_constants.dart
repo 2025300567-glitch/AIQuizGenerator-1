5b44d73caf41040ab0a7c9e2900c36073ec41020
Kyran-Git <niklauscareer@gmail.com>
2025-11-19 23:34:50 +0800

    Add GeminiConstants for API key management; update google_generative_ai dependency to version 0.4.7

---
+++ ai_quiz_generator/lib/constants/gemini_constants.dart
@@ -0,0 +1,3 @@
+class GeminiConstants {
+  static const String API_KEY = "AIzaSyB84Y0Qxa5434nwNdPyE7HJS-iHblXQ5AI";
+}
--- ai_quiz_generator/lib/controller/ai_controller.dart
+++ ai_quiz_generator/lib/controller/ai_controller.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:ai_quiz_generator/constants/gemini_constants.dart';
+
+import 'dart:convert';
+import 'dart:developer';
 
 class AiController extends GetxController {
   TextEditingController createQuizExplanation = TextEditingController();
+
+  Future createQuiz() async {
+    // AI quiz creation logic here
+    try {
+      log("AiController :: createQuiz()");
+      final model = GenerativeModel(
+        model: 'gemini-2.0-flash',
+        apiKey: GeminiConstants.API_KEY,
+      );
+    } catch (e) {
+      log("AiController :: createQuiz() :: Error:$e");
+    }
+  }
 }
